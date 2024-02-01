c94f3d70756de5eaca8141e8e04a348e928a0dd4
prohashim <prohashim4e@gmail.com>
2024-02-01 15:49:44 +0500

    first commit for module 18

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(require("./routes"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+});
